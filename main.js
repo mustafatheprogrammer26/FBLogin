cb9712f52187f9be6e56a068b262964cfc38c8e0
unknown <MustafaRazaShaikh>
2024-06-01 16:56:36 +0500

    Facebook login page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ login.addEventListener("click", () => {
     let userEmail = document.getElementById("email").value;
     let userPassword = document.getElementById("password").value;
     if (userEmail == "mustafa778822@gmail.com" && userPassword == 201145) {
-      window.location.href = "https://github.com/MustafaRazaShaikh";
+      window.location.href = "https://www.facebook.com/";
     } else {
       alert("Please try again");
     }
